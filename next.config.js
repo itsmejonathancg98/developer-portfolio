d2f3363e527ff65424a397debb5dfabd3ca26cc0
itsmejonathancg98 <chanukagurusinghe@gmail.com>
2023-08-29 17:33:05 +0530

    changed the path 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: false,
   basePath: '/developer-portfolio',
+  assetPrefix: '/developer-portfolio/'
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
